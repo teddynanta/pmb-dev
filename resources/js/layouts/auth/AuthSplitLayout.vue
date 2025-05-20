f3048f3b50c44335a460c9dfc74e8af90ec8d6a4
Teddy Nanta <teddy.nanta99@gmail.com>
2025-05-20 11:05:35 +0700

    add navbar and hero section (not yet finish)

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
-import { Link, usePage } from '@inertiajs/vue3';
+    import AppLogoIcon from '@/components/AppLogoIcon.vue';
+    import { Link, usePage } from '@inertiajs/vue3';
 
-const page = usePage();
-const name = page.props.name;
-const quote = page.props.quote;
+    const page = usePage();
+    const name = page.props.name;
+    const quote = page.props.quote;
 
-defineProps<{
-    title?: string;
-    description?: string;
-}>();
+    defineProps<{
+        title?: string;
+        description?: string;
+    }>();
 </script>
 
 <template>
-    <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div
+        class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div class="absolute inset-0 bg-zinc-900" />
+            <div class="absolute inset-0 bg-stiebi" />
             <Link :href="route('home')" class="relative z-20 flex items-center text-lg font-medium">
-                <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
-                {{ name }}
+            <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
+            <p class="ml-5">{{ name }}</p>
             </Link>
             <div v-if="quote" class="relative z-20 mt-auto">
                 <blockquote class="space-y-2">
@@ -38,3 +39,13 @@ defineProps<{
         </div>
     </div>
 </template>
+
+<style>
+    .bg-stiebi {
+        background-image: url('../../../images/bg.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        filter: brightness(50%);
+    }
+
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-    import { Head, Link } from '@inertiajs/vue3';
+    import { Head, Link, usePage } from '@inertiajs/vue3';
     import { Icon } from '@iconify/vue'
     import { useColorMode } from '@vueuse/core'
     import { Button } from '@/components/ui/button'
     import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+    import { BookOpenIcon } from 'lucide-vue-next';
 
     // Pass { disableTransition: false } to enable transitions
-    const mode = useColorMode()
+    const mode = useColorMode();
+    const page = usePage();
+    const name = page.props.name;
 </script>
 
 <template>
@@ -18,10 +21,10 @@
     <div
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
         <header
-            class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl flex items-center justify-between gap-10">
+            class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-6xl flex items-center justify-between gap-10">
             <div class="flex flex-row items-center gap-2">
                 <img src="../../images/logo.png" alt="logo" class="h-10">
-                <span class="font-bold">STIEBI PRANA PUTRA</span>
+                <span class="font-bold dark:text-white">{{ name }}</span>
             </div>
             <nav class="flex items-center justify-end gap-4">
                 <Link v-if="$page.props.auth.user" :href="route('dashboard')"
@@ -29,13 +32,17 @@
                 Dashboard
                 </Link>
                 <template v-else>
-                    <Link :href="route('register')"
-                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]">
-                    Register
+                    <Link :href="route('dashboard')"
+                        class="inline-block leading-normal hover:text-primary dark:text-white">
+                    Kontak
+                    </Link>
+                    <Link :href="route('dashboard')"
+                        class="inline-block leading-normal hover:text-primary dark:text-white">
+                    Persyaratan
                     </Link>
                     <Button as-child class="bg-primary">
-                        <a href="/login">
-                            Login
+                        <a href="/register">
+                            Daftar Sekarang!
                         </a>
                     </Button>
                 </template>
@@ -66,8 +73,26 @@
         <div
             class="duration-750 starting:opacity-0 flex w-full items-center justify-center opacity-100 transition-opacity lg:grow">
             <main
-                class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row">
-                <h1 class="dark:text-[#EDEDEC]">hello</h1>
+                class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-6xl lg:flex-row">
+                <div class="w-1/2 flex items-center">
+                    <p class="font-black text-6xl dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit.
+                        Quibusdam,
+                        unde.
+                    </p>
+                </div>
+                <div class="w-1/2 relative inline-block">
+                    <div class="absolute inset-0 bg-primary z-2 h-3/4 w-1/2 mx-auto mt-auto rounded-2xl"></div>
+                    <div
+                        class="absolute inset-5 outline-1 outline-primary z-1 h-3/4 w-1/2 ms-auto me-14 mt-2 rounded-2xl">
+                    </div>
+                    <div
+                        class="flex items-center border-1 border-green-200 absolute bg-white dark:bg-black p-2 text-sm font-black rounded-xl ms-12 mt-32 w-auto z-5">
+                        <BookOpenIcon class="h-4 inline" />
+                        <p>Kelas Karyawan</p>
+                    </div>
+                    <img src="../../images/mhs.png" alt="mahasiswa" class="relative max-w-3/4 mx-auto -mt-14 z-10">
+                </div>
             </main>
         </div>
         <div class="h-14.5 hidden lg:block"></div>
