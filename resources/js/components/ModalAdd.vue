c5d2809cf32036956d4fc65fe2d7b979e18e8f00
Teddy Nanta <teddy.nanta99@gmail.com>
2025-05-28 11:54:52 +0700

    add modal for add user

---
+++ resources/js/components/ModalAdd.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+    import { Button } from '@/components/ui/button'
+    import {
+        Dialog,
+        DialogContent,
+        DialogDescription,
+        DialogFooter,
+        DialogHeader,
+        DialogTitle,
+        DialogTrigger,
+    } from '@/components/ui/dialog'
+    import { Plus } from "lucide-vue-next";
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger as-child>
+            <Button class="cursor-pointer">
+                <Plus class="w-4 h-4 mr-2" />
+                <slot name="button-slot"></slot>
+            </Button>
+
+        </DialogTrigger>
+        <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>
+                    <slot name="title-slot"></slot>
+                </DialogTitle>
+                <DialogDescription>
+                    <slot name="desc-slot"></slot>
+                </DialogDescription>
+            </DialogHeader>
+
+            <slot name="form-slot"></slot>
+
+            <DialogFooter>
+                <Button class="cursor-pointer" type="submit" form="dialogForm">
+                    <slot name="button-slot"></slot>
+                </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
--- resources/js/pages/Users/Index.vue
+++ resources/js/pages/Users/Index.vue
@@ -2,13 +2,37 @@
     import AppLayout from '@/layouts/AppLayout.vue';
     import { type BreadcrumbItem } from '@/types';
     import { Head } from '@inertiajs/vue3';
-    import type { User } from '@/pages/Users/column'
-    import { ref } from 'vue'
-    import { columns } from '@/pages/Users/column'
-    import DataTable from '@/components/ui/data-table/DataTable.vue'
-    import { Button } from "@/components/ui/button";
-    import { Plus } from "lucide-vue-next";
+    import type { User } from '@/pages/Users/column';
+    import { ref } from 'vue';
+    import { columns } from '@/pages/Users/column';
+    import DataTable from '@/components/ui/data-table/DataTable.vue';
+    // import { Button } from "@/components/ui/button";
+    // import { Plus } from "lucide-vue-next";
+    import ModalAdd from '@/components/ModalAdd.vue';
+    import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from '@/components/ui/form';
+    import { Input } from '@/components/ui/input';
+    import { toTypedSchema } from '@vee-validate/zod';
+    import * as z from 'zod';
+    import { toast } from 'vue-sonner';
+    import { Toaster } from '@/components/ui/sonner'
+    import 'vue-sonner/style.css'
 
+
+    const formSchema = toTypedSchema(z.object({
+        username: z.string().min(5).max(10),
+        name: z.string().min(5).max(10),
+        email: z.string().email(),
+    }))
+
+    function onSubmit() {
+        toast('You submitted the following values:', {
+            description: 'Sunday, December 03, 2023 at 9:00 AM',
+            action: {
+                label: 'Undo',
+                onClick: () => console.log('Undo'),
+            },
+        })
+    }
     const breadcrumbs: BreadcrumbItem[] = [
         {
             title: 'Users',
@@ -19,8 +43,6 @@
     const props = defineProps<{ users: User[] }>();
     const tableData = ref([...props.users]);
     function handleRowDelete(id: number) {
-        console.log('Deleting user with ID:', id);
-
         tableData.value = tableData.value.filter(user => user.id !== id);
     }
 
@@ -35,14 +57,48 @@
     <Head title="Users" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
+        <Toaster class="pointer-events-auto" />
         <div class="container py-4 px-5 mx-auto">
-            <div class="flex items-center py-4">
-                <Button as-child>
-                    <a href="users/create">
-                        <Plus class="w-4 h-4 mr-2" /> Add User
-                    </a>
-                </Button>
-            </div>
+            <Form v-slot="{ handleSubmit }" as="" keep-values :validation-schema="formSchema">
+                <ModalAdd>
+                    <template #button-slot>
+                        Add User
+                    </template>
+                    <template #title-slot>Add User</template>
+                    <template #desc-slot>Fill in the details to add a new user.</template>
+                    <template #form-slot>
+                        <form id="dialogForm" @submit="handleSubmit($event, onSubmit)">
+                            <FormField v-slot="{ componentField }" name="username">
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input type="text" placeholder="shadcn" v-bind="componentField" />
+                                    </FormControl>
+                                    <FormMessage class="text-red-500" />
+                                </FormItem>
+                            </FormField>
+                            <FormField v-slot="{ componentField }" name="name">
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input type="text" placeholder="shadcn" v-bind="componentField" />
+                                    </FormControl>
+                                    <FormMessage class="text-red-500" />
+                                </FormItem>
+                            </FormField>
+                            <FormField v-slot="{ componentField }" name="email">
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" placeholder="shadcn" v-bind="componentField" />
+                                    </FormControl>
+                                    <FormMessage class="text-red-500" />
+                                </FormItem>
+                            </FormField>
+                        </form>
+                    </template>
+                </ModalAdd>
+            </Form>
             <DataTable :columns="columnsData" :data="tableData" :on-row-delete="handleRowDelete" />
         </div>
     </AppLayout>
